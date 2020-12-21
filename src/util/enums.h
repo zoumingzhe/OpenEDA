6ace7c0b4cc5ae73028daa4ac86ea136b662a8b3
haoqs <haoqs@niiceda.com>
2020-12-21 22:34:21 +0800

    use toupper and tolower

--- src/util/enums.h
+++ src/util/enums.h
@@ -26,36 +26,28 @@ namespace util {
 inline void toLower(char *s) {
     int len = strlen(s);
     for (int i = 0; i < len; i++) {
-        if (s[i] >= 'A' && s[i] <= 'Z') {
-            s[i] += 32;  //+32 to lower case
-        }
+        s[i] = tolower(s[i]);
     }
 }
 
 inline void toLower(std::string &s) {
     int len = s.size();
     for (int i = 0; i < len; i++) {
-        if (s[i] >= 'A' && s[i] <= 'Z') {
-            s[i] += 32;  //+32 to lower case
-        }
+        s[i] = tolower(s[i]);
     }
 }
 
 inline void toUpper(std::string &s) {
     int len = s.size();
     for (int i = 0; i < len; i++) {
-        if (s[i] >= 'a' && s[i] <= 'z') {
-            s[i] -= 32;  //-32 to upper case
-        }
+        s[i] = toupper(s[i]);
     }
 }
 
 inline void toUpper(char *s) {
     int len = strlen(s);
     for (int i = 0; i < len; i++) {
-        if (s[i] >= 'a' && s[i] <= 'z') {
-            s[i] -= 32;  //-32 to upper case
-        }
+        s[i] = toupper(s[i]);
     }
 }
 /// @brief Types of models.
